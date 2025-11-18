b1d7354e08da4338215637316199353358a8196b
nmgd-ixd <132451761+nmgd-ixd@users.noreply.github.com>
2025-11-18 15:33:35 +0900

    .

--- aca/main.js
+++ aca/main.js
@@ -1,7 +1,122 @@
-import { Bodies, Body, Engine, Events, Render, Runner, World } from "matter-js";
-import { FRUITS_BASE, ITEMS_ACA } from "./fruits";
+//import { Bodies, Body, Engine, Events, Render, Runner, World } from "matter-js";
+//import { FRUITS_BASE, ITEMS_ACA } from "./fruits";
+
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Body = Matter.Body,
+    Runner = Matter.Runner,
+    Events = Matter.Events; // 시뮬레이션 루프를 위한 Runner 모듈 추가
+
+
+const ITEMS_ACA = [
+  {
+    name: "aca/00_item",
+    radius: 33 / 2,
+  },
+  {
+    name: "aca/01_item",
+    radius: 43 / 2,
+  },
+  {
+    name: "aca/02_item",
+    radius: 61 / 2,
+  },
+  {
+    name: "aca/03_item",
+    radius: 76 / 2,
+  },
+  {
+    name: "aca/04_item",
+    radius: 95 / 2,
+  },
+  {
+    name: "aca/05_item",
+    radius: 117 / 2,
+  },
+  {
+    name: "aca/06_item",
+    radius: 137 / 2,
+  },
+  {
+    name: "aca/07_item",
+    radius: 161 / 2,
+  },
+  {
+    name: "aca/08_item",
+    radius: 204 / 2,
+  },
+  {
+    name: "aca/09_item",
+    radius: 220 / 2,
+  },
+  {
+    name: "aca/10_item",
+    radius: 260 / 2,
+  },
+];
+
+const FRUITS_BASE = [
+  {
+    name: "base/00_cherry",
+    radius: 33 / 2,
+  },
+  {
+    name: "base/01_strawberry",
+    radius: 48 / 2,
+  },
+  {
+    name: "base/02_grape",
+    radius: 61 / 2,
+  },
+  {
+    name: "base/03_gyool",
+    radius: 69 / 2,
+  },
+  {
+    name: "base/04_orange",
+    radius: 89 / 2,
+  },
+  {
+    name: "base/05_apple",
+    radius: 114 / 2,
+  },
+  {
+    name: "base/06_pear",
+    radius: 129 / 2,
+  },
+  {
+    name: "base/07_peach",
+    radius: 156 / 2,
+  },
+  {
+    name: "base/08_pineapple",
+    radius: 177 / 2,
+  },
+  {
+    name: "base/09_melon",
+    radius: 220 / 2,
+  },
+  {
+    name: "base/10_watermelon",
+    radius: 259 / 2,
+  },
+];
+
+
+
+
+
+
+
+
+
+
+
 
 let THEME = "aca"; // { base, aca }
+
 let FRUITS = FRUITS_BASE;
 
 const scoreText = document.getElementById('score');
